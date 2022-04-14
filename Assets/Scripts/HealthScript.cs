fa3c51525c72a6d901e53e32457811a2ca2e26f9
Isaac Redlon <isaacredlon@gmail.com>
2022-04-14 00:43:19 -0400

    Combined separate prefabs into a build scene

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+    public int maxHealth = 1000;
+    public int health;
+    
+    public Slider healthSlider;
+    
+    void Start()
+    {
+        health = maxHealth;
+    }
+    
+    public void TakeDamage(int dmg) {
+        Debug.Log("dmg taken: " + dmg);
+        
+        health -= dmg;
+        healthSlider.value = (float)health / (float)maxHealth;
+    }
+}
